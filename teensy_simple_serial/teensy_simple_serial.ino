d05ff7136ec29b071cd1f68d69ffd26bc9a6816c
BasicMicro <info@basicmicro.com>
2019-07-02 18:15:09 -0700

    Added comments

--- teensy_simple_serial/teensy_simple_serial.ino
+++ teensy_simple_serial/teensy_simple_serial.ino
@@ -1,19 +1,22 @@
+// Use the harware serial port #1 and start it at a buadrate of 38400bps 
 void setup() {
   Serial1.begin(38400);
 }
 
 void loop() {
-  //Turn motor1 forward at half speed
+  // Turn motor1 forward at half speed
   Serial1.write(94);
   delay(2000);
-  //Stop motor1
+  
+  // Stop motor1
   Serial1.write(64);
   delay(2000);
-  //Turn motor1 backwards at half speed
+  
+  // Turn motor1 backwards at half speed
   Serial1.write(32);
   delay(2000);
-  //Stop motor1
+  
+  // Stop motor1
   Serial1.write(64);
   delay(2000);
-
 }
